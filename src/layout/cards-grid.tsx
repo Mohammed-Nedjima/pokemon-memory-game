75b3e93cc9313d2e6c5f5dc32ea109315883f44d
Nedjima Mohammed Ayoub <mm_nedjima@esi.dz>
2025-08-14 19:23:20 +0100

    responsive design fixes

--- src/layout/cards-grid.tsx
+++ src/layout/cards-grid.tsx
@@ -66,7 +66,7 @@ const CardsGrid = () => {
         <h2 className="text-2xl font-bold">Current Score: {currentScore}</h2>
         <h2 className="text-2xl font-bold">High Score: {highScore}</h2>
       </div>
-      <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {pokemonList.map((pokemon) => (
           <Card
             key={pokemon.name}
